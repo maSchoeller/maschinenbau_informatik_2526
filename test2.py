7ee2c7d7febad424a53a3032ab3f7741dc760fdb
Marvin Schoeller <git@marvinschoeller.de>
2025-12-02 13:37:44 +0100

    Change addition to subtraction in test2.py

--- test2.py
+++ test2.py
@@ -1,5 +1,5 @@
 zahl1 = str(30)
 zahl2 = input("Gib die zweite Zahl ein: ")
 # hier wird zahl1 mit zahl2 addiert
-summe = float(zahl1) + float(zahl2) # die eigentliche addition
-print("Die Summe der beiden Zahlen ist:", summe)
+summe = float(zahl1) - float(zahl2) # die eigentliche addition
+print("Die nicht Summe der beiden Zahlen ist:", summe)
